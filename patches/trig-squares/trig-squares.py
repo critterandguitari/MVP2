b7c0e4758f17441fb35dbdf2c0f3741ee073d236
Critter & Guitari <critter@critterandguitari.com>
2016-10-25 18:48:10 +0000

    stuff from last time

--- patches/trig-squares/trig-squares.py
+++ patches/trig-squares/trig-squares.py
@@ -2,6 +2,7 @@ import os
 import pygame
 import time
 import random
+import pygame.gfxdraw
 
 note_down = False
 
@@ -15,4 +16,5 @@ def square(screen):
     y = random.randrange(0,screen.get_height())
     color = (random.randrange(0,255), random.randrange(0,255), random.randrange(0,255))
     #pygame.draw.line(screen, color, [x, y], [x,y+100 ], 100)
-    pygame.draw.rect(screen, color, [x, y, x+100,y+100 ], 10)
+    #pygame.draw.rect(screen, color, [x, y, x+100,y+100 ], 10)
+    pygame.gfxdraw.box(screen, [x, y, x+100,y+100 ], color)
